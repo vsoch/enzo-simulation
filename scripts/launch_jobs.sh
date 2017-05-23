1ea61cd979d21aab4b0a271c9a8fe94ad48fb32c
vsoch <vsochat@stanford.edu>
2017-05-23 15:32:50 -0400

    adding script to launch jobs

---
+++ scripts/launch_jobs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Load singularity
+module load singularity
+enzo="/scratch/users/vsochat/DATA/enzo.img"
+
+# Get number of jobs
+if [ $# -lt 1 ]; then
+    echo "You need to pass the number of jobs."
+    exit 1
+fi;
+
+num="$1"
+
+#User input validation
+case ${num} in
+    ''|*[!0-9]*) echo "You must pass a number as the first argument."; exit 2;;
+     *) ;;
+esac
+
+host_data_dir="/scratch/users/vsochat/DATA/physics"
+compiler_version="1.10.2"
+compiler_vendor="gcc"
+optimization_level="high"
+export host_data_dir compiler_vendor compiler_version optimization_level
+
+job=0
+while [ ${job} -lt ${num} ]; do
+    job_dir="${host_data_dir}/jobs/${job}"
+    if [ ! -e "${job_dir}/RunFinished" ]; then
+        cd "${job_dir}"
+        echo "Submitting job ${job_num}..."
+        qsub -N "${job_type}_${compiler_vendor}_${compiler_version}_${optimization_level}_${job_num}" -d ${job_dir} singularity run $enzo $job_num 
+	fi;
+	job=$(expr ${job} + 1)
+done;
