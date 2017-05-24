380ae4f6e57e9f380ee07ef636a711a4ea9f99a5
vsoch <vsochat@stanford.edu>
2017-05-24 18:31:20 -0400

    modified:   scripts/run_job.sh

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -7,6 +7,7 @@ module load openmpi/1.10.2/gcc
 
 job_num=$1
 
+host_data="/scratch/users/vsochat/DATA/physics"
 enzo="/scratch/users/vsochat/DATA/enzo.img"
 
-singularity run $enzo $job_num
+singularity run -B $host_data:/data $enzo $job_num
